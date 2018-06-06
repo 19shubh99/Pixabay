90c9f0af95af01fe0775acdca356529924cc8573
Your Name <you@example.com>
2018-06-06 21:06:05 +0300

    working with pixabay api

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import NavBar from './components/navbar/NavBar';
+import Search from './components/search/Search';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      
+      <MuiThemeProvider>
+        <div>
+
+          <NavBar/>
+          <Search/>
+        </div>
+
+        </MuiThemeProvider>
+    );
+     
+
+    
+  }
+}
+
+export default App;
